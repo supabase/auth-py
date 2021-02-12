e7539278fbcbef3a05971856d20a2a1587a9049e
leonfedden <leonfedden@gmail.com>
2021-02-12 11:41:08 +0000

    bringing closer to pairity with gotrue js

--- gotrue/__init__.py
+++ gotrue/__init__.py
@@ -1,3 +1,5 @@
-__version__ = '0.1.0'
+__version__ = '0.2.0'
 
+from . import client
+from . import api
 from .client import Client
--- gotrue/client.py
+++ gotrue/client.py
@@ -3,9 +3,12 @@ client.py
 ====================================
 core module of the project
 """
+import functools
+import json
 import requests
 import re
-import json
+import uuid
+from typing import Any, Callable, Dict, Optional
 from urllib.parse import quote
 
 HTTPRegexp = "/^http://"
@@ -17,61 +20,211 @@ def jsonify(dictionary: dict):
 
 
 class Client:
-    def __init__(self, url, audience="", setCookie=False, headers={}):
+    def __init__(
+        self,
+        headers: Dict[str, str],
+        url: str = GOTRUE_URL,
+        detect_session_in_url: bool = True,
+        auto_refresh_token: bool = True,
+        persist_session: bool = True,
+        local_storage: Dict[str, Any] = {},
+        cookie_options: Dict[str, Any] = {},
+    ):
+        """Create a new client for use in the browser.
+
+        url
+            The URL of the GoTrue server.
+        headers
+            Any additional headers to send to the GoTrue server.
+        detectSessionInUrl
+            Set to "true" if you want to automatically detects OAuth grants in
+            the URL and signs in the user.
+        autoRefreshToken
+            Set to "true" if you want to automatically refresh the token before
+            expiring.
+        persistSession
+            Set to "true" if you want to automatically save the user session
+            into local storage.
+        localStorage
+        """
         if re.match(HTTPRegexp, url):
-            # TODO: Decide whether to convert this to a logging statement
             print(
-                "Warning:\n\nDO NOT USE HTTP IN PRODUCTION FOR GOTRUE EVER!\nGoTrue REQUIRES HTTPS to work securely."
+                "Warning:\n\nDO NOT USE HTTP IN PRODUCTION FOR GOTRUE EVER!\n"
+                "GoTrue REQUIRES HTTPS to work securely."
             )
-        self.BASE_URL = url
-        self.headers = headers
+        self.state_change_emitters: Dict[str, Any] = {}
+        self.current_user = None
+        self.current_session = None
+        self.auto_refresh_token = auto_refresh_token
+        self.persist_session = persist_session
+        self.local_storage: Dict[str, Any] = {}
+        self.api = GoTrueApi(url=url, headers=headers, cookie_options=cookie_options)
+        self._recover_session()
 
-    def settings(self):
-        """Get environment settings for the server"""
-        return requests.get(f"{self.BASE_URL}/settings", headers=self.headers)
+    def sign_up(self, email: str, password: str):
+        """Creates a new user.
 
-    def sign_up(self, credentials: dict):
-        return requests.post(
-            f"{self.BASE_URL}/signup", jsonify(credentials), headers=self.headers
+        Paramters
+        ---------
+        email : str
+            The user's email address.
+        password : str
+            The user's password.
+        """
+        self._remove_session()
+        data = self.api.sign_up(email, password)
+        if data.user.confirmed_at:
+            self._save_session(data)
+            self._notify_all_subscribers("SIGNED_IN")
+        return data
+
+    def sign_in(
+        self,
+        email: Optional[str] = None,
+        password: Optional[str] = None,
+        provider: Optional[str] = None,
+    ) -> Optional[Dict[str, Any]]:
+        """Log in an exisiting user, or login via a third-party provider."""
+        self._remove_session()
+        if email is not None and password is None:
+            self.api.send_magic_link_email(email)
+            data = None
+        elif email is not None and password is not None:
+            data = self._handle_email_sign_in(email, password)
+        elif provider is not None:
+            data = self._handle_provider_sign_in(provider)
+        else:
+            raise ValueError("Email or provider must not be None.")
+        return data
+
+    def user(self) -> Optional[Dict[str, Any]]:
+        """Returns the user data, if there is a logged in user."""
+        return self.current_user
+
+    def session(self) -> Optional[Dict[str, Any]]:
+        """Returns the session data, if there is an active session."""
+        return self.current_session
+
+    def refresh_session(self) -> Dict[str, Any]:
+        """Force refreshes the session.
+
+        Force refreshes the session including the user data incase it was
+        updated in a different session.
+        """
+        if self.current_session is None or not self.current_session.access_token:
+            raise ValueError("Not logged in.")
+        self._call_refresh_token()
+        data = self.api.get_user(self.current_session.access_token)
+        self.current_user = data
+        return data
+
+    def update(self, **attributes) -> Dict[str, Any]:
+        """Updates user data, if there is a logged in user."""
+        if self.current_session is None or not self.current_session.get("access_token"):
+            raise ValueError("Not logged in.")
+        data = self.api.update_user(self.current_session["access_token"], **attributes)
+        self.current_user = data
+        self._notify_all_subscribers("USER_UPDATED")
+        return data
+
+    def get_session_from_url(self, store_session: bool):
+        """Gets the session data from a URL string."""
+        raise NotImplementedError(
+            "This method is a stub and is only required by the JS client."
         )
 
-    def sign_in(self, credentials: dict):
-        """Sign in with email and password"""
-        return self.grant_token("password", credentials, headers=self.headers)
+    def sign_out(self):
+        """Log the user out."""
+        if self.current_session is not None and "access_token" in self.current_session:
+            self.api.sign_out(self.current_session["access_token"])
+        self._remove_session()
+        self._notify_all_subscribers("SIGNED_OUT")
 
-    def refresh_access_token(self, refresh_token: str):
-        return grant_token("refresh_token", {"refresh_token": refresh_token})
+    def on_auth_state_change(
+        self, callback: Callable[[str, Optional[Dict[str, Any]]], Any],
+    ):
+        """"""
+        unique_id: str = str(uuid.uuid4())
+        subscription: Dict[str, Any] = {
+            "id": unique_id,
+            "callback": callback,
+            "unsubscribe": functools.partial(
+                self.state_change_emitters.pop, id=unique_id
+            ),
+        }
+        self.state_change_emitters[unique_id] = subscription
+        return subscription
 
-    def grant_token(self, type: str, data: dict):
-        return requests.post(
-            f"{self.BASE_URL}/token?grant_type=#{type}/", jsonify(data)
-        )
+    def _handle_email_sign_in(self, email: str, password: str) -> Dict[str, Any]:
+        """Sign in with email and password."""
+        data = self.api.sign_in_with_email(email, password)
+        if (
+            data is not None
+            and data.get("user") is not None
+            and "confirmed_at" in data["user"]
+        ):
+            self._save_session(data)
+            self._notify_all_subscribers("SIGNED_IN")
+        return data
 
-    def sign_out(jwt: str):
-        """Sign out user using a valid JWT"""
-        return requests.post(f"{self.BASE_URL}/logout", auth=jwt)
+    def _handle_provider_sign_in(self, provider):
+        """Sign in with provider."""
+        raise NotImplementedError("Not implemeted for Python client.")
 
-    def recover(self, email: str):
-        """ Send a recovery email """
-        data = {"email": email}
-        return requests.post(f"{self.BASE_URL}/recover", jsonify(data))
+    def _save_session(self, session):
+        """Save session to client."""
+        self.current_session = session
+        self.current_user = session.user
+        token_expiry_seconds = session.get("expires_in")
+        if self.auto_refresh_token and token_expiry_seconds is not None:
+            self._set_timeout(
+                self._call_refresh_token, (token_expiry_seconds - 60) * 1000
+            )
+        if self.persist_session:
+            self._persist_session(self.current_session, token_expiry_seconds)
 
-    def get_user(self, jwt: str):
-        """Get user info using a valid JWT"""
-        return requests.get(f"{self.BASE_URL}/user", auth=jwt)
+    def _persist_session(self, current_session, seconds_to_expiry: int):
+        timenow_seconds: int = datetime.datetime.now()
+        expires_at: int = timenow_seconds + seconds_to_expiry
+        data = {
+            "current_session": current_session,
+            "expires_at": expires_at,
+        }
+        self.local_storage[STORAGE_KEY] = json.dumps(data)
 
-    def update_user(self, jwt: str, info: dict):
-        """Update user info using a valid JWT"""
-        return requests.put(f"{self.BASE_URL}/user", auth=jwt, data=info)
+    def _remove_session(self):
+        """Remove the session."""
+        self.current_session = None
+        self.current_user = None
+        self.local_storage.pop(STORAGE_KEY, None)
 
-    def send_magic_link(self, email: str):
-        """Send a magic link for passwordless login"""
-        data = json.dumps({"email": email})
-        return requests.post(f"{self.BASE_URL}/magiclink", data=data)
+    def _recover_session(self):
+        """Kept as a stub for pairity with the JS client. Only required for
+        React Native.
+        """
+        pass
 
-    def url_for_provider(self, provider: str) -> str:
-        return f"{self.BASE_URL}/authorize?provider=#{urllib.parse.quote(provider)}"
+    def _call_refresh_token(self, refresh_token: Optional[str] = None):
+        logged_in: bool = self.current_session is not None and "access_token" in self.current_session
+        if refresh_token is None and logged_in:
+            refresh_token = self.current_session["refresh_token"]
+        elif refresh_token is None:
+            raise ValueError("No current session and refresh_token not supplied.")
+        data = self.api.refresh_access_token(refresh_token)
+        if data is not None and "access_token" in data:
+            self.current_session = data
+            self.current_user = data.user
+            self._notify_all_subscribers("SIGNED_IN")
+            token_expiry_seconds = data.get("expires_in")
+            if self.auto_refresh_token and token_expiry_seconds is not None:
+                self._set_timeout(
+                    self._call_refresh_token, (token_expiry_seconds - 60) * 1000
+                )
+            if self.persist_session:
+                self._persist_session(self.current_session, token_expiry_seconds)
+        return data
 
-    def invite(self, invitation: dict):
-        """Invite a new user to join"""
-        return requests.post(f"{self.BASE_URL}/invite", jsonify(invitation))
+    def _notify_all_subscribers(self, event: str):
+        """Notify all subscribers that auth event happened."""
+        for value in self.state_change_emitters.values():
+            value["callback"](event, self.current_session)
