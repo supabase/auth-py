f70fdd200c2b676069e262d18a4d3f1cb8b4d99a
leynier <leynier41@gmail.com>
2021-10-23 02:44:23 -0400

    feat: implement and use cli for unasync

--- build_sync.py
+++
@@ -1,7 +0,0 @@
-from setuptools import find_packages, setup
-from unasync import cmdclass_build_py
-
-setup(
-    packages=find_packages(),
-    cmdclass={"build_py": cmdclass_build_py()},  # type: ignore
-)
