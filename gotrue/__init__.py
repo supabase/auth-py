8d2c74e5592f6f64dccd40116661ccfc97c88f0e
Leynier Gutiérrez González <leynier41@gmail.com>
2021-11-14 04:28:29 +0000

    chore: remove noqa comments

--- gotrue/__init__.py
+++ gotrue/__init__.py
@@ -2,13 +2,13 @@ from __future__ import annotations
 
 __version__ = "0.2.0"
 
-from ._async.api import AsyncGoTrueAPI  # noqa: F401
-from ._async.client import AsyncGoTrueClient  # noqa: F401
-from ._async.storage import AsyncMemoryStorage, AsyncSupportedStorage  # noqa: F401
-from ._sync.api import SyncGoTrueAPI  # noqa: F401
-from ._sync.client import SyncGoTrueClient  # noqa: F401
-from ._sync.storage import SyncMemoryStorage, SyncSupportedStorage  # noqa: F401
-from .types import *  # noqa: F401, F403
+from ._async.api import AsyncGoTrueAPI
+from ._async.client import AsyncGoTrueClient
+from ._async.storage import AsyncMemoryStorage, AsyncSupportedStorage
+from ._sync.api import SyncGoTrueAPI
+from ._sync.client import SyncGoTrueClient
+from ._sync.storage import SyncMemoryStorage, SyncSupportedStorage
+from .types import *
 
 Client = SyncGoTrueClient
 GoTrueAPI = SyncGoTrueAPI
