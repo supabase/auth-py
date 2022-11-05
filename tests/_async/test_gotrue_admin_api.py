8c076f0d23411b0ee1861b91739f45069275c630
Leynier Gutiérrez González <leynier41@gmail.com>
2022-11-05 06:27:05 +0000

    test: add test to user fetch methods of admin api

--- tests/_async/test_gotrue_admin_api.py
+++ tests/_async/test_gotrue_admin_api.py
@@ -1,4 +1,4 @@
-from .clients import service_role_api_client
+from .clients import auth_client_with_session, service_role_api_client
 from .utils import (
     create_new_user_with_email,
     mock_app_metadata,
@@ -59,3 +59,35 @@ async def test_create_user_with_user_and_app_metadata():
     assert "profile_image" in response.user.user_metadata
     assert "provider" in response.user.app_metadata
     assert "providers" in response.user.app_metadata
+
+
+async def test_list_users_should_return_registered_users():
+    credentials = mock_user_credentials()
+    await create_new_user_with_email(email=credentials.get("email"))
+    users = await service_role_api_client().list_users()
+    assert users
+    emails = [user.email for user in users]
+    assert emails
+    assert credentials.get("email") in emails
+
+
+async def test_get_user_fetches_a_user_by_their_access_token():
+    credentials = mock_user_credentials()
+    auth_client_with_session_current_user = auth_client_with_session()
+    response = await auth_client_with_session_current_user.sign_up(
+        {
+            "email": credentials.get("email"),
+            "password": credentials.get("password"),
+        }
+    )
+    assert response.session
+    response = await auth_client_with_session_current_user.get_user()
+    assert response.user.email == credentials.get("email")
+
+
+async def test_get_user_by_id_should_a_registered_user_given_its_user_identifier():
+    credentials = mock_user_credentials()
+    user = await create_new_user_with_email(email=credentials.get("email"))
+    assert user.id
+    response = await service_role_api_client().get_user_by_id(user.id)
+    assert response.user.email == credentials.get("email")
