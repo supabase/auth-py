91b0597e77efe4e5b3ad11c18188f6121fe75d20
joel@joellee.org <joel@joellee.org>
2023-01-22 23:20:23 +0800

    chore: patch module level import

--- gotrue/__init__.py
+++ gotrue/__init__.py
@@ -2,13 +2,13 @@ from __future__ import annotations
 
 __version__ = "0.5.4"
 
-from ._async.api import AsyncGoTrueAPI
 from ._async.client import AsyncGoTrueClient
+from ._async.gotrue_admin_api import AsyncGoTrueAdminAPI
 from ._async.storage import AsyncMemoryStorage, AsyncSupportedStorage
-from ._sync.api import SyncGoTrueAPI
 from ._sync.client import SyncGoTrueClient
+from ._sync.gotrue_admin_api import SyncGoTrueAdminAPI
 from ._sync.storage import SyncMemoryStorage, SyncSupportedStorage
 from .types import *
 
 Client = SyncGoTrueClient
-GoTrueAPI = SyncGoTrueAPI
+GoTrueAPI = SyncGoTrueAdminAPI
