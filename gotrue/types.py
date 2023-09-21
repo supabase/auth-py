da51e8ea262fa113da7bd77c4e67ba14c7262851
Andrew Smith <a.smith@silentworks.co.uk>
2023-09-21 00:40:56 +0000

    fix: add verify token hash

--- gotrue/types.py
+++ gotrue/types.py
@@ -41,6 +41,15 @@ Provider = Literal[
     "zoom",
 ]
 
+EmailOtpType = Literal[
+    "signup",
+    "invite",
+    "magiclink",
+    "recovery",
+    "email_change",
+    "email"
+]
+
 AuthChangeEventMFA = Literal["MFA_CHALLENGE_VERIFIED"]
 
 AuthChangeEvent = Literal[
@@ -325,13 +334,7 @@ class VerifyOtpParamsOptions(TypedDict):
 class VerifyEmailOtpParams(TypedDict):
     email: str
     token: str
-    type: Literal[
-        "signup",
-        "invite",
-        "magiclink",
-        "recovery",
-        "email_change",
-    ]
+    type: EmailOtpType
     options: NotRequired[VerifyOtpParamsOptions]
 
 
@@ -344,10 +347,15 @@ class VerifyMobileOtpParams(TypedDict):
     ]
     options: NotRequired[VerifyOtpParamsOptions]
 
+class VerifyTokenHashParams(TypedDict):
+    token_hash: str
+    type: EmailOtpType
+    options: NotRequired[VerifyOtpParamsOptions]
 
 VerifyOtpParams = Union[
     VerifyEmailOtpParams,
     VerifyMobileOtpParams,
+    VerifyTokenHashParams
 ]
 
 
