d54feeb21006bfd5e5e1bdc039fac0aadab24e65
Andrew Smith <a.smith@silentworks.co.uk>
2024-09-02 11:20:00 +0000

    feat: add reauthenticate method (#586)

--- supabase_auth/_async/gotrue_client.py
+++ supabase_auth/_async/gotrue_client.py
@@ -541,6 +541,18 @@ class AsyncGoTrueClient(AsyncGoTrueBaseAPI):
             self._notify_all_subscribers("SIGNED_IN", response.session)
         return response
 
+    async def reauthenticate(self) -> AuthResponse:
+        session = await self.get_session()
+        if not session:
+            raise AuthSessionMissingError()
+
+        return await self._request(
+            "GET",
+            "reauthenticate",
+            jwt=session.access_token,
+            xform=parse_auth_response,
+        )
+
     async def get_session(self) -> Union[Session, None]:
         """
         Returns the session, refreshing it if necessary.
