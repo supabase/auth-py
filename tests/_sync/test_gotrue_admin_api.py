9837eb49c95a9cecbb6ba11a05b8d965a89f2986
Juan Carlos <juancarlospaco@gmail.com>
2025-01-06 23:50:23 -0300

    fix: fix asserts and assert messages in tests

--- tests/_sync/test_gotrue_admin_api.py
+++ tests/_sync/test_gotrue_admin_api.py
@@ -150,14 +150,14 @@ def test_modify_confirm_email_using_update_user_by_id():
         }
     )
     assert response.user
-    assert not response.user.email_confirmed_at
+    assert not response.user.confirmed_at
     response = service_role_api_client().update_user_by_id(
         response.user.id,
         {
             "email_confirm": True,
         },
     )
-    assert response.user.email_confirmed_at
+    assert response.user.confirmed_at
 
 
 def test_delete_user_should_be_able_delete_an_existing_user():
@@ -254,7 +254,7 @@ def test_verify_otp_with_non_existent_phone_number():
         )
         assert False
     except AuthError as e:
-        assert e.message == "User not found"
+        assert e.message == "Token has expired or is invalid"
 
 
 def test_verify_otp_with_invalid_phone_number():
