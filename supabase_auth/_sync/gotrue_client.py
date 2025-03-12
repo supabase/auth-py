c001f79b22c9d4c60862cf8d6397aaeee850dda6
Guilherme Souza <guilherme@supabase.io>
2025-03-12 11:55:00 +0100

    fix code format

--- supabase_auth/_sync/gotrue_client.py
+++ supabase_auth/_sync/gotrue_client.py
@@ -423,9 +423,7 @@ class SyncGoTrueClient(SyncGoTrueBaseAPI):
         )
         return OAuthResponse(provider=provider, url=url_with_qs)
 
-    def link_identity(
-        self, credentials: SignInWithOAuthCredentials
-    ) -> OAuthResponse:
+    def link_identity(self, credentials: SignInWithOAuthCredentials) -> OAuthResponse:
         provider = credentials.get("provider")
         options = credentials.get("options", {})
         redirect_to = options.get("redirect_to")
@@ -708,9 +706,7 @@ class SyncGoTrueClient(SyncGoTrueBaseAPI):
         self._notify_all_subscribers("TOKEN_REFRESHED", session)
         return AuthResponse(session=session, user=response.user)
 
-    def refresh_session(
-        self, refresh_token: Optional[str] = None
-    ) -> AuthResponse:
+    def refresh_session(self, refresh_token: Optional[str] = None) -> AuthResponse:
         """
         Returns a new session, regardless of expiry status.
 
@@ -1119,9 +1115,7 @@ class SyncGoTrueClient(SyncGoTrueBaseAPI):
         if self._flow_type == "pkce":
             code_verifier = generate_pkce_verifier()
             code_challenge = generate_pkce_challenge(code_verifier)
-            self._storage.set_item(
-                f"{self._storage_key}-code-verifier", code_verifier
-            )
+            self._storage.set_item(f"{self._storage_key}-code-verifier", code_verifier)
             code_challenge_method = (
                 "plain" if code_verifier == code_challenge else "s256"
             )
